b0b213ce04189de388aae0ce407147384d5cb335
Marek Bečvář <markbecvar@gmail.com>
2021-06-20 10:52:42 +0200

    init push

---
+++ nvim/init.vim
@@ -0,0 +1,5 @@
+source $HOME/.config/nvim/plugins.vim
+source $HOME/.config/nvim/settings.vim
+source $HOME/.config/nvim/mappings.vim
+
+lua require'plug-colorizer'
---
+++ zsh/prompt.zsh
@@ -0,0 +1,6 @@
+PROMPT="%B%{$fg[blue]%}[%{$fg[white]%}%"+mark"%{$fg[red]%}@%{$fg[white]%}%"+rog"%{$fg[blue]%}]%{$reset_color%}%  %(?:%{$fg_bold[green]%}➜ :%{$fg_bold[red]%}➜ )"
+PROMPT+=' %{$fg[cyan]%}%c%{$reset_color%} '
+ZSH_THEME_GIT_PROMPT_PREFIX="%{$fg_bold[blue]%}(%{$fg[red]%}"
+ZSH_THEME_GIT_PROMPT_SUFFIX="%{$reset_color%} "
+ZSH_THEME_GIT_PROMPT_DIRTY="%{$fg[blue]%}) %{$fg[yellow]%}✗"
+ZSH_THEME_GIT_PROMPT_CLEAN="%{$fg[blue]%})"
